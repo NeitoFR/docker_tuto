8923f812c02e1f2ea7e8814ddfbbf1f1ceeb0fbc
Wille Houm <willem.houm@gmail.com>
2018-10-17 18:04:20 +0200

    Init of a Polymer Docker application

--- webapp/app/server/app.js
+++ webapp/app/server/app.js
@@ -1,26 +1,23 @@
 "use strict"
-//require
-require('dotenv').config();
 
-var path = require('path'),
-    bodyParser = require('body-parser'),
-    express = require('express'),
-    http = require('http');
+var express = require('express'),
+    path = require('path');
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/..', 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
 app.get('/', function (req, res) {
     res.sendFile('../public/index.html', { root: __dirname });
+
+    //console.log(req.url);
 });
 
-app.listen(9002, function () {
-    console.log('Server listening on port : ' + 9002);
+
+app.listen(5000, function () {
+    console.log('Server listening on  : ' + 5000);
 });
